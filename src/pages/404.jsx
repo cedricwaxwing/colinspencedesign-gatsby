353f058e3408f7ea246843e70ee8ad8f3584db27
Colin Spence <spencec6@gmail.com>
2020-09-08 20:36:01 -0600

    404 updates

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link } from '@components/Link'
 
 import { Layout } from '@components/Layout'
 import { Text } from '@components/Text'
-import { Br } from '@components/Grid'
+import { Br, Box, Flex } from '@components/Grid'
 
 const NotFoundPage = () => (
   <Layout>
-    <Text as="h2" mb={3}>
-      Not found
-    </Text>
-    <Text>
-      You just hit a route that doesn&#39;t exist... the sadness.
-      <Br />
-      <Link to="/">Take me home.</Link>
-    </Text>
+    <Flex minHeight="100vh" alignItems="center" justifyContent="center">
+      <Box>
+        <Text as="h2" fontSize={[5,6,7]} mb={2} textTransform="uppercase">
+          This page was not found.
+        </Text>
+        <Text>
+          You just hit a route that doesn&#39;t exist... the sadness.
+          <Br />
+          <Link to="/" mt={2}>Take me home.</Link>
+        </Text>
+      </Box>
+    </Flex>
   </Layout>
 )
 
