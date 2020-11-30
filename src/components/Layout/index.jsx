6ba7336c2c34c4c9fcb947921b90757fb00c08c6
Colin Spence <spencec6@gmail.com>
2020-11-29 21:36:00 -0700

    hero updates

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import GoToTop from '@components/GoToTop'
+import { Box, Flex } from '@components/Grid'
 
 import { Text } from '@components/Text'
 import GlobalStyles from '@style/GlobalStyles'
@@ -24,27 +26,32 @@ const Layout = ({ children, isLive, prev, current, next, path, props }) => (
     />
     <main>{children}</main>
     <Footer>
-      <Text fontSize={2}>2009 - {year} © Colin Spence Design</Text>
-      <Text fontSize={1} color="grey.300">
-        With help along the way from
-        {` `}
-        <AniLink
-          paintDrip
-          duration={0.5}
-          fontSize={1}
-          from="footer"
-          to="/finefolks"
-          color="grey.200"
-          css={{
-            textDecoration: 'underline',
-            '&:hover': {
-              color: 'white',
-            },
-          }}
-        >
-          these mighty fine folks.
-        </AniLink>
-      </Text>
+    <Flex justifyContent="space-between" alignItems="center">
+        <Box mr={2}>
+        <Text fontSize={2}>2009 - {year} © Colin Spence Design</Text>
+        <Text fontSize={1} color="grey.300">
+          With help along the way from
+          {` `}
+          <AniLink
+            paintDrip
+            duration={0.5}
+            fontSize={1}
+            from="footer"
+            to="/finefolks"
+            color="grey.200"
+            css={{
+              textDecoration: 'underline',
+              '&:hover': {
+                color: 'white',
+              },
+            }}
+          >
+            these mighty fine folks.
+          </AniLink>
+        </Text>
+        </Box>
+        {isLive ? <GoToTop/> : null}
+      </Flex>
     </Footer>
   </ThemeProvider>
 )
